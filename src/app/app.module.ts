ca135461bc20a92363c3d7ab50390e484dfaa3be
douglasrg1 <douglasrg1@outlook.com.br>
2019-05-14 12:38:13 -0300

    criado algumas pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/Shared/header/header.component';
 import { SubMenuComponent } from './Components/Shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+import { ProductListComponent } from './Components/product-list/product-list.component';
+import { HomePageComponent } from './Components/Pages/home-page/home-page.component';
+import { LoginPageComponent } from './Components/Pages/login-page/login-page.component';
+import { SignupPageComponent } from './Components/Pages/signup-page/signup-page.component';
+import { CartPageComponent } from './Components/Pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SubMenuComponent
+    SubMenuComponent,
+    FooterComponent,
+    ProductListComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
